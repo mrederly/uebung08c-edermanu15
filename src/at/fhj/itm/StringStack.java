5471792337fd539a8ccd7c8790e5499b87722154
mrederly <mr.ederly@gmx.at>
2016-06-03 12:35:26 +0200

    first commit

---
+++ src/at/fhj/itm/StringStack.java
@@ -0,0 +1,31 @@
+package at.fhj.itm;
+
+/**
+ * Stack Implementation of <code>Stack</code> Interface.
+ * supports String Values and can be initialized with a maximum number
+ * of items.
+ *  
+ * @see Stack
+ */
+public class StringStack implements Stack
+{
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(String item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
